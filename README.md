4555073500f4d7d82a44aa12aab2d5bee1d3c62a
cjihrig <cjihrig@gmail.com>
2015-04-20 10:03:28 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # usps
-USPS API wrapper
+
+[![Current Version](https://img.shields.io/npm/v/usps.svg)](https://www.npmjs.org/package/usps)
+[![Build Status via Travis CI](https://travis-ci.org/cjihrig/usps.svg?branch=master)](https://travis-ci.org/cjihrig/usps)
+![Dependencies](http://img.shields.io/david/cjihrig/usps.svg)
+![devDependencies](http://img.shields.io/david/dev/cjihrig/usps.svg)
+
+JavaScript API wrapper for the United States Postal Service (USPS) APIs. The USPS APIs are
+XML based. This module abstracts away the details of working with XML.
+
+This module exports a constructor function used to interact with the various APIs. This
+design decision was made because the USPS API requests require a user ID. Instead of
+passing this ID to every request, it is simpler to construct an object that retains the ID.
+If you need to make requests using multiple user IDs, simply construct another instance of
+this module (in the future, it might be possible to override the user ID on a per call
+basis).
+
+## Basic Usage
+
+```javascript
+var Usps = require('usps');
+var usps = new Usps({userId: 'your_usps_id'});
+var options = {zip: '10001'};
+
+usps.cityStateLookup(options, function(err, results) {
+  // results = {zip: '10001', city: 'NEW YORK', state: 'NY'}
+});
+```
+
+### Running the Tests
+
+Run the following command:
+
+```
+USPS_USER_ID=YOUR_USPS_ID npm test
+```
+
+Where `YOUR_USPS_ID` is your USPS user ID.
+
+## `Usps(options)` Constructor
+
+  - Arguments
+    - `options` (object) - An object containing the following configuration settings:
+        - `userId` (string) - A USPS user ID.
+
+Returns a new instance of the USPS API.
+
+## Methods
+
+### `cityStateLookup(options, callback)`
+
+  - Arguments
+    - `options` (object) - An object containing the following configuration settings:
+        - `zip` (string or array) - A zip code string, or an array of zip code strings.
+    - `callback` (function) - Callback function triggered after a response is received from the USPS services. This function takes the following arguments:
+        - `err` (`Error` or `null`) - If an error is encountered while looking up any of the provided zip codes, it is reported here. If no error is encountered, this is `null`.
+        - `results` (object or array) - If a string was passed in `zip`, this will be an object containing the zip code and corresponding city and state. If `zip` was an array, this will be an array of objects.
+  - Returns
+    - Nothing. See `callback` function argument.
+
+Given one or more zip codes, this method returns the corresponding city and state(s).
---
+++ test/index.js
@@ -0,0 +1,149 @@
+'use strict';
+var Code = require('code');
+var Lab = require('lab');
+var Usps = require('../');
+
+var lab = exports.lab = Lab.script();
+var expect = Code.expect;
+var describe = lab.describe;
+var it = lab.it;
+
+Code.settings.truncateMessages = false;
+
+var userId = process.env.USPS_USER_ID;
+
+
+describe('USPS API', function() {
+  describe('Usps()', function() {
+    it('can construct with the new keyword', function(done) {
+      var usps = new Usps({userId: userId});
+
+      expect(usps._userId).to.equal(userId);
+      expect(usps._host).to.exist();
+      expect(usps._secureHost).to.exist();
+      done();
+    });
+
+    it('can construct without the new keyword', function(done) {
+      var usps = Usps({userId: userId});
+
+      expect(usps._userId).to.equal(userId);
+      expect(usps._host).to.exist();
+      expect(usps._secureHost).to.exist();
+      done();
+    });
+
+    it('rejects invalid options', function(done) {
+      function fail(arg) {
+        return function() {
+          new Usps(arg);
+        };
+      }
+
+      expect(fail(null)).to.throw(Error, 'options must be an object');
+      expect(fail(undefined)).to.throw(Error, 'options must be an object');
+      expect(fail(1)).to.throw(Error, 'options must be an object');
+      expect(fail(true)).to.throw(Error, 'options must be an object');
+      expect(fail(false)).to.throw(Error, 'options must be an object');
+      expect(fail('foo')).to.throw(Error, 'options must be an object');
+      expect(fail({})).to.throw(Error, 'user ID must be a string');
+      done();
+    });
+  });
+
+  describe('cityStateLookup()', function() {
+    it('successfully looks up a zip code', function(done) {
+      var usps = new Usps({userId: userId});
+      var options = {zip: '10001'};
+
+      usps.cityStateLookup(options, function(err, results) {
+        expect(err).to.not.exist();
+        expect(results).to.deep.equal({
+          zip: '10001',
+          city: 'NEW YORK',
+          state: 'NY'
+        });
+        done();
+      });
+    });
+
+    it('handles error responses', function(done) {
+      var usps = new Usps({userId: userId});
+      var options = {zip: '?'};
+
+      usps.cityStateLookup(options, function(err, results) {
+        expect(err).to.exist();
+        expect(err.message).to.equal('ZIPCode must be 5 characters');
+        expect(results).to.equal(err);
+        done();
+      });
+    });
+
+    it('looks up multiple zip codes at once', function(done) {
+      var usps = new Usps({userId: userId});
+      var options = {zip: ['90210', '10001']};
+
+      usps.cityStateLookup(options, function(err, results) {
+        expect(err).to.not.exist();
+        expect(results).to.deep.equal([
+          {zip: '90210', city: 'BEVERLY HILLS', state: 'CA'},
+          {zip: '10001', city: 'NEW YORK', state: 'NY'}
+        ]);
+        done();
+      });
+    });
+
+    it('handles error responses in multi zip code lookup', function(done) {
+      var usps = new Usps({userId: userId});
+      var options = {zip: ['?', '10001']};
+
+      usps.cityStateLookup(options, function(err, results) {
+        expect(err).to.exist();
+        expect(err.message).to.equal('ZIPCode must be 5 characters');
+        expect(results).to.be.an.array();
+        expect(results.length).to.equal(2);
+        expect(results[0]).to.equal(err);
+        expect(results[1]).to.deep.equal({
+          zip: '10001',
+          city: 'NEW YORK',
+          state: 'NY'
+        });
+        done();
+      });
+    });
+
+    it('rejects invalid options', function(done) {
+      function fail(options) {
+        return function() {
+          var usps = new Usps({userId: userId});
+
+          usps.cityStateLookup(options, function() {});
+        };
+      }
+
+      expect(fail(null)).to.throw(Error, 'options must be an object');
+      expect(fail(undefined)).to.throw(Error, 'options must be an object');
+      expect(fail(1)).to.throw(Error, 'options must be an object');
+      expect(fail(true)).to.throw(Error, 'options must be an object');
+      expect(fail(false)).to.throw(Error, 'options must be an object');
+      expect(fail('foo')).to.throw(Error, 'options must be an object');
+      expect(fail({zip: 1})).to.throw(Error, 'zips must be a string or array with at least one element');
+      expect(fail({zip: []})).to.throw(Error, 'zips must be a string or array with at least one element');
+      done();
+    });
+
+    it('handles unreachable USPS server', function(done) {
+      var usps = new Usps({userId: userId, host: 'foo'});
+      var options = {zip: '10001'};
+
+      usps.cityStateLookup(options, function(err, results) {
+        expect(err).to.exist();
+        expect(err.code).to.equal('ECONNREFUSED');
+        expect(err.errno).to.equal('ECONNREFUSED');
+        expect(err.syscall).to.equal('connect');
+        expect(results).to.not.exist();
+        done();
+      });
+    });
+  });
+});
